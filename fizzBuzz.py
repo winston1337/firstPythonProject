aa52a8266b85d9fd6ff8cdc8cab9477776760c68
Winston <wmchetz95@gmail.com>
2025-01-14 23:59:06 -0500

    fizzbuzz

---
+++ fizzBuzz.py
@@ -0,0 +1,11 @@
+print("let's do fizzbuzz")
+
+for num in range(1,101):
+    string = ""
+    if num % 3 == 0:
+        string = string + "Fizz"
+    if num % 4 == 0:
+        string = string + "Buzz"
+    if num % 4 != 0 and num % 3 != 0:
+        string = string + str(num)
+    print(string)
